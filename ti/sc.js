0368a54925866b3d2139e64eba38c0ee53d7367c
Mochigomedango <115089857+Mochigomedango@users.noreply.github.com>
2022-10-06 19:55:31 +0800

    Add files via upload

--- ti/sc.js
+++ ti/sc.js
@@ -4,9 +4,10 @@ bodyBgs[1] = "../images/02.jpg";
 var randomBgIndex = Math.round( Math.random() * 1 );
 document.write('<style>body{background:url(' + bodyBgs[randomBgIndex] + ') no-repeat 50% 0}</style>');
 var link = document.createElement('link');
-link.rel = 'icon';
+link.rel = 'shortcut icon';
 link.href = '../favicon.ico';
-    document.getElementsByTagName('head')[0].appendChild(link);
+link.type =  'image/x-icon';
+document.getElementsByTagName('head')[0].appendChild(link);
 window.onload = function(){
     var w = Math.max(document.documentElement.clientWidth,document.body.scrollWidth);
     var h = Math.max(document.documentElement.clientHeight,document.body.scrollHeight);
